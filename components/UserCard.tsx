6e547f7fbed9456ff489e77d59f34ec0bb85b3ae
Shamaz saeed <shamazsaeed332@gmail.com>
2024-12-11 18:39:27 +0000

    navigation added from user listing to user profile page

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { User } from '../types/user'
+import Link from 'next/link'
 
 type UserCardProps = {
 	user: User
 }
 
 export default function UserCard({ user }: UserCardProps) {
-	return (
+    return (
+        <Link href={`/profile/${user.id}`}>
 		<li className='border p-4 rounded shadow flex items-center space-x-4'>
 			<Image
 				src={user.profileImage}
@@ -18,6 +20,7 @@ export default function UserCard({ user }: UserCardProps) {
 				<h2 className='text-xl font-semibold'>{user.name}</h2>
 				<p className='text-gray-600'>{user.bio}</p>
 			</div>
-		</li>
+            </li>
+        </Link>
 	)
 }
